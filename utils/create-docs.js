a896cc1301cd248ebd4356fd03454dac194276ed
Ibz <ibaq@duck.com>
2024-04-19 16:04:47 +0100

    add a link to preview.html

--- utils/create-docs.js
+++ utils/create-docs.js
@@ -11,6 +11,16 @@ const packages = [];
 
 const ignoreDirs = ['0.test'];
 
+let extendedContent = `
+  
+### Documentation
+
+This file is generated through an Github Action automation, if any of the image previews do not load. There is an issue with the action.  
+
+## Available backgrounds
+
+`;
+
 function readDirectory(dir) {
   const files = fs.readdirSync(dir);
   for (const file of files) {
@@ -22,18 +32,20 @@ function readDirectory(dir) {
     if (stats.isDirectory()) {
       readDirectory(filePath);
     } else if (file === 'package.yaml') {
+
       const packageData = YAML.parse(fs.readFileSync(filePath, 'utf8'));
       const author = packageData.metadata.author;
       authors[author] = (authors[author] || 0) + 1;
-      // Get the directory name containing the package.yaml file
       const packageName = path.basename(path.dirname(filePath));
 
-      let content = `
+      extendedContent += `
 
 ###  ${packageName} 
 ${packageData.metadata.name} - ${packageData.metadata.description} by ${packageData.metadata.author}
 
 ![Image Preview](https://ibz0q.github.io/lovelace-bg-animation/gallery/metadata/${packageName}/screenshot.png)
+
+Live Preview: [preview.html](https://ibz0q.github.io/lovelace-bg-animation/gallery/metadata/${packageName}/preview.html)
 
 Place this inside your config: 
       
@@ -43,13 +55,13 @@ Place this inside your config:
 
 
 `
-      packages.push(content);
     }
   }
 }
 
 readDirectory(galleryDir);
 
+// Readme.md
 const sortedAuthors = Object.entries(authors)
   .sort((a, b) => b[1] - a[1]);
 
@@ -57,7 +69,6 @@ const html = sortedAuthors
   .map(([author, count]) => ` - ${author} (${count})`)
   .join('\n');
 
-  console.log(html);
 const readmePath = path.join(__dirname, '../README.md');
 let readmeContent = fs.readFileSync(readmePath, 'utf8');
 
@@ -67,27 +78,12 @@ readmeContent = readmeContent.replace(regex, `$1\n\n${html}\n$3`);
 
 // Write the updated content back to the README.md file
 fs.writeFileSync(readmePath, readmeContent, 'utf8');
+
+
 // Check if the EXTENDED.MD file exists
 if (!fs.existsSync(documentationPath)) {
   // If it doesn't exist, create it with the initial content
-  
-  let content = `
-  
-### Documentation
-
-This file is generated through an Github Action automation, if any of the image previews do not load. There is an issue with the action.  
-
-## Available backgrounds
-
-`;
-
-
   fs.writeFileSync(documentationPath, content, 'utf8');
 }
 
-// Write the package names to the EXTENDED.MD file
-let documentationContent = fs.readFileSync(documentationPath, 'utf8');
-regex = /(## Available backgrounds\n)([\s\S]*)/;
-documentationContent = documentationContent.replace(regex, `$1${packages.join('\n')}`);
-
-fs.writeFileSync(documentationPath, documentationContent, 'utf8');
+fs.writeFileSync(documentationPath, extendedContent, 'utf8');
